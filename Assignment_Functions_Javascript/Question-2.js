b1edcd0c9991ea02339dfa370ab6ef42ff35b67d
Nilesh Suresh Kulkarni <168301773+enileshk@users.noreply.github.com>
2025-06-01 01:23:29 +0530

    Create Question-2.js

---
+++ Assignment_Functions_Javascript/Question-2.js
@@ -0,0 +1,20 @@
+// Function to generate a personalized greeting
+function generateGreeting(name) {
+return `Hello, ${name}! Welcome to our website.`;
+}
+
+// Using the function to greet three different people
+const greeting1 = generateGreeting("Nilesh");
+const greeting2 = generateGreeting("Nikhil");
+const greeting3 = generateGreeting("Abir");
+
+// Display the greetings
+console.log(greeting1);
+console.log(greeting2);
+console.log(greeting3);
+
+Output:
+
+Hello, Nilesh! Welcome to our website.
+Hello, Nikhil! Welcome to our website.
+Hello, Abir! Welcome to our website.

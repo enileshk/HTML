360a28cbf9d3bd88ce1408d7624f333aee587058
Nilesh Suresh Kulkarni <168301773+enileshk@users.noreply.github.com>
2025-06-01 00:31:28 +0530

    Create Question-5.js

---
+++ Assignment-Conditions and Loops/Question-5.js
@@ -0,0 +1,15 @@
+// Ascending order: 1 to 25
+let i = 1;
+console.log("Ascending order:");
+while (i <= 25) {
+  console.log(i);
+  i++;
+}
+
+// Descending order: 25 to 1
+let j = 25;
+console.log("Descending order:");
+while (j >= 1) {
+  console.log(j);
+  j--;
+}

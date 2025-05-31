baa947561a501633123e3dc677c28870945baa00
Nilesh Suresh Kulkarni <168301773+enileshk@users.noreply.github.com>
2025-06-01 01:17:41 +0530

    Create QUestion-1.js

---
+++ Assignment_Functions_Javascript/QUestion-1.js
@@ -0,0 +1,9 @@
+// Arrow function to calculate square
+const square = number => number * number;
+
+// Example usage
+const num = 5;
+const result = square(num);
+
+console.log(The square of ${num} is ${result}); //Output:The square of 5 is 25
+
